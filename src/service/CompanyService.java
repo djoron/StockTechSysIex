479c07c4d850e35240878fc9bbc50156d67c2ab5
djoron <djoron@yahoo.ca>
2018-01-06 18:31:56 -0500

    Minor corrections to companyServuce
    See title

--- src/service/CompanyService.java
+++ src/service/CompanyService.java
@@ -57,20 +57,66 @@ public class CompanyService {
             if (!companyList.isEmpty()) 
             {
                 // Save in SQL DB.
-                logger.info("createStockList - getCompanyList. Downloaded {} elements.",companyList.size());
-                logger.info("createStockList - getCompanyList. Saving into DB.",companyList.size());
+                logger.info("createCompanyList: Downloaded {} elements.",companyList.size());
+                logger.info("createCompanyList: getCompanyList. Saving into DB.",companyList.size());
                 CompanyDao companyDao = new CompanyDaoImpl();
                 companyDao.saveCompanyList(companyList);
 
             } else {
                 // If returns 0 no data so cannot build stocklist. Must exit
-                logger.info("createStockList - getCompanyList: Could not be built.");
+                logger.info("createStockList: getCompanyList: Could not be built.");
                 return false;
             }
         } else
         {
             // If returns 0 no data so cannot build stocklist. Must exit
-            logger.info("getSymbolList: Could not download any data...exciting");
+            logger.info("createCompanyList: Could not download any data...exciting");
+            return false;
+        }
+
+        
+        return true;
+        
+    }
+
+        /**
+     * Update Company list. Calls internet DAOs to do so and saves it into
+     * SQLDb for persistence.
+     * @return True if download and save ok. False otherwise.
+     * @throws Exception
+     */
+    public boolean updateCompanyList() throws Exception {
+        boolean status;
+        
+        // Dao to access internat Data
+        IexDao iexDao = new IexDaoImpl() {};
+        
+        // Will contain symbol only List from Internet.
+        List<Symbol> symbolList = iexDao.getSymbolList();
+        
+        // Use internet to download full Symbol List to populate new DB 
+        if (!symbolList.isEmpty()) 
+        {
+            // We have data. Build company List, download info from internet.
+            List<Company> companyList = iexDao.getCompanyList(symbolList);
+            
+            if (!companyList.isEmpty()) 
+            {
+                // Save in SQL DB.
+                logger.info("updateCompanyList: Downloaded {} elements.",companyList.size());
+                logger.info("updateCompanyList: Saving into DB.",companyList.size());
+                CompanyDao companyDao = new CompanyDaoImpl();
+                companyDao.updateCompanyList(companyList);
+
+            } else {
+                // If returns 0 no data so cannot build stocklist. Must exit
+                logger.info("updateCompanyList: getCompanyList: Could not be built.");
+                return false;
+            }
+        } else
+        {
+            // If returns 0 no data so cannot build stocklist. Must exit
+            logger.info("updateCompanyList: Could not download any data...exciting");
             return false;
         }
 
